37af374aaa597e93a94889f566dc736f5f23d739
Kilian Kluge <kluge@physik.uni-bonn.de>
2017-12-07 15:37:51 +0100

    Add helper function to generate list of Hamiltonians for parallelized mesolve

--- lib/parallelize.py
+++ lib/parallelize.py
@@ -13,13 +13,15 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+import itertools
 import os
 from multiprocessing import Pool
 
+import numpy as np
 import qutip
 
 
-def mesolve(list_of_H, psi0, times, c_ops, e_ops):
+def mesolve(hamiltonians, psi0, times, c_ops, e_ops):
     """Wrapper for parallel evaluation of `qutip.mesolve`.
 
     Instead of a single Hamiltonian, a list of Hamiltonians to be
@@ -27,7 +29,46 @@ def mesolve(list_of_H, psi0, times, c_ops, e_ops):
 
     All other arguments are identical to `qutip.mesolve`.
     """
-    worklist = [(H, psi0, times, c_ops, e_ops) for H in list_of_H]
+    worklist = [(H, psi0, times, c_ops, e_ops) for H in hamiltonians]
     with Pool(processes=os.cpu_count()) as p:
         results = p.starmap(qutip.mesolve, worklist)
     return results
+
+
+def vary(system, parameters=(), **kwargs):
+    """Generate a list of Hamiltonians for use in `parallelize.mesolve`."""
+    if not parameters:
+        if len(kwargs) > 1:
+            raise ValueError(
+                "Multiple parameters need to be passed as a list to guarantee a non-ambiguous order.")
+        elif len(kwargs) == 0:
+            raise ValueError("No parameter list and no single parameter given.")
+        else:
+            parameters = [(parameter, range_) for parameter, range_ in kwargs.items()]
+
+    for parameter, range_ in parameters:
+        try:
+            _ = getattr(system, parameter)
+        except AttributeError:
+            raise AttributeError("System has no parameter '%s'." % parameter)
+
+    expanded_parameters = [itertools.product([parameter], range_) for parameter, range_ in parameters]
+    parameter_space = itertools.product(*expanded_parameters)
+
+    hamiltonians = []
+    system_parameters = []
+
+    for parameter_set in parameter_space:
+        for parameter, value in parameter_set:
+            try:
+                setattr(system, parameter, value)
+            except AttributeError:
+                raise AttributeError("System has no settable parameter '%s'." % parameter)
+            else:
+                hamiltonians.append(system.H)
+        system_parameters.append([value for parameter, value in parameter_set])
+
+    if np.all([np.isclose(hamiltonians[0].full(), H.full()) for H in hamiltonians[1:]]):
+        raise ValueError("All generated Hamiltonians are identical.")
+
+    return hamiltonians, system_parameters
