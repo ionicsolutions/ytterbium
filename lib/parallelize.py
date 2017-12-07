9e62f3221d8413e84fff112de07159e41465ffab
Kilian Kluge <kluge@physik.uni-bonn.de>
2017-12-08 00:24:13 +0100

    Greatly simplify the syntax of lib.parallelize.vary by exploiting Python 3.6 feature

--- lib/parallelize.py
+++ lib/parallelize.py
@@ -35,48 +35,44 @@ def mesolve(hamiltonians, psi0, times, c_ops, e_ops):
     return results
 
 
-def vary(system, parameters=(), **kwargs):
+def vary(system, **kwargs):
     """Generate a list of Hamiltonians for use in `parallelize.mesolve`.
 
-    If only one parameter is to be varied, the parameter and the values it
-    should take on can be provided as a keyword argument:
+    The parameters and their values are provided as keyword arguments:
 
-    >>> vary(TwoLevelSystem, delta=np.linspace(-10.0, 10.0, num=30))
+    .. code-block: python
 
-    When several parameters vary, they need to be provided as a list or
-    tuple which defines the order of the Hamiltonians:
-
-    >>> vary(TwoLevelSystem, parameters=(("delta", [-100.0, -10.0, 0.0]), ("sat", [0.1, 1.0])))
+       hamiltonians, parameters = vary(TwoLevelSystem(),
+                                       delta=np.linspace(-10.0, 10.0, num=30),
+                                       sat=[0.1, 1.0])
 
     The function returns two values:
-    The first (*hamiltonians*) is the list of Hamiltonians which can be passed to `parallelize.mesolve`.
-    The second (*system_parameters*) is a list of the parameter values used to generate the Hamiltonians:
+    - *hamiltonians* is the list of Hamiltonians which can be passed to
+       `parallelize.mesolve`.
+    - *system_parameters* is a list of the same length where each entry
+       contains the parameter values of the Hamiltonian.
 
-    >>> hamiltonians, system_parameters = vary(...)
-    >>> system_parameters
-    [[-100.0, 0.1], [-100.0, 1.0], [-10.0, 0.1], [-10.0, 1.0], [0.0, 0.1], [0.0, 1.0]]
+    The parameters are processed in the order in which they are passed
+    to the function, and Hamiltonians for all possible combinations are
+    generated.
     """
-    if not parameters:
-        if len(kwargs) > 1:
-            raise ValueError(
-                "Multiple parameters need to be passed as a list to guarantee a non-ambiguous order.")
-        elif len(kwargs) == 0:
-            raise ValueError("No parameter list and no single parameter given.")
-        else:
-            parameters = [(parameter, range_) for parameter, range_ in kwargs.items()]
+    if not kwargs:
+        raise ValueError("No parameter to vary.")
+    else:
+        variable_parameters = [(parameter, range_) for parameter, range_ in kwargs.items()]
 
     initial = {}  # check that all parameters exist and record their initial value
-    for parameter, range_ in parameters:
+    for parameter, range_ in variable_parameters:
         try:
             initial[parameter] = getattr(system, parameter)
         except AttributeError:
             raise AttributeError("System has no parameter '%s'." % parameter)
 
-    expanded_parameters = [itertools.product([parameter], range_) for parameter, range_ in parameters]
+    expanded_parameters = [itertools.product([parameter], range_) for parameter, range_ in variable_parameters]
     parameter_space = itertools.product(*expanded_parameters)
 
     hamiltonians = []
-    system_parameters = []
+    parameters = []
 
     for parameter_set in parameter_space:
         for parameter, value in parameter_set:
@@ -86,7 +82,7 @@ def vary(system, parameters=(), **kwargs):
                 raise AttributeError("Parameter '%s' cannot be set." % parameter)
             else:
                 hamiltonians.append(system.H)
-        system_parameters.append([value for parameter, value in parameter_set])
+        parameters.append([value for parameter, value in parameter_set])
 
     if np.all([np.isclose(hamiltonians[0].full(), H.full()) for H in hamiltonians[1:]]):
         raise ValueError("All generated Hamiltonians are identical.")
@@ -94,4 +90,4 @@ def vary(system, parameters=(), **kwargs):
     for parameter, value in initial.items():  # reset system to avoid confusing behavior
         setattr(system, parameter, value)
 
-    return hamiltonians, system_parameters
+    return hamiltonians, parameters
